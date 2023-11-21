08f5f8b929468efc80e3b49cba4fa51eb62e9a80
sskkilm <sskkilm@naver.com>
2023-11-22 00:01:16 +0900

    Initial Commit

---
+++ src/main/java/com/example/finiensserverjava/FiniensServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.finiensserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniensServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiniensServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finiensserverjava/FiniensServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finiensserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiniensServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
